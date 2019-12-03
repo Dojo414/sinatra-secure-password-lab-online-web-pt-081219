e22ac1eff0c7f9389bb345a109841ec87b8a2442
Donovan Jones <donovanljones94@gmail.com>
2019-12-03 15:44:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    
+    unless params[:username] || params[:password] == ""
     user = User.new(:username => params[:username], :password => params[:password])
-    
-    if user.save && user.username != ""
       redirect '/login'
     else
       binding.pry
