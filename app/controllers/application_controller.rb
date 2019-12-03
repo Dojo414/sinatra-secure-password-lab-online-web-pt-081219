c2b1a576846745ad1fa8706519785fab9acfd606
Donovan Jones <donovanljones94@gmail.com>
2019-12-03 15:47:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     unless params[:username] || params[:password] == ""
     user = User.new(:username => params[:username], :password => params[:password])
+    binding.pry
       redirect '/login'
     else
       redirect '/failure'
